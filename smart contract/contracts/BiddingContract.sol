e2a88381a448d125be2c5cfc3db4dae7cfe5f68f
Aditya <aditya.bonde1152@gmail.com>
2024-09-22 01:40:40 +0530

    Smart Contract Updated: Auction Functionalities added

--- smart contract/contracts/BiddingContract.sol
+++ smart contract/contracts/BiddingContract.sol
@@ -13,14 +13,14 @@ interface IERC721 {
 contract NFTAuction {
     uint256 private constant Duration = 7 days;
 
-    IERC721 public immutable nft;
-    uint256 public immutable nftId;
+    IERC721 public  nft;
+    uint256 public  nftId;
 
     address payable public  seller;
-    uint256 public immutable startingPrice;
-    uint256 public immutable discountRate;
-    uint256 public immutable startAt;
-    uint256 public immutable expiresAt;
+    uint256 public  startingPrice;
+    uint256 public  discountRate;
+    uint256 public  startAt;
+    uint256 public  expiresAt;
 
     constructor(
         uint256 _startingPrice,
@@ -34,6 +34,38 @@ contract NFTAuction {
         startAt = block.timestamp;
         expiresAt = block.timestamp + Duration;
 
+        require(_startingPrice >= _discountRate + Duration, "Starting Price is too low");
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
     }
+
+
+        function getPrice() public view returns(uint256) {
+                uint256 timeElapsed = block.timestamp - startAt;
+                uint256 discount = discountRate * timeElapsed;
+
+                return startingPrice - discount;
+
+        }
+
+        function buy( ) external payable {
+            require(block.timestamp < expiresAt , "This NFT Auction has ended");
+
+            uint256 price = getPrice();
+            require(msg.value >= price , "Amount of ETH sent is less than the price");
+
+            nft.transferFrom(seller, msg.sender, nftId);
+            uint refund = msg.value - price;
+
+            if(refund > 0){
+                payable(msg.sender).transfer(refund);
+
+            }
+            selfdestruct(seller);
+            
+        }
+
+    
     
 }
